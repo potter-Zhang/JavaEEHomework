0db00c1268e8203ea47c63db48b11ab04b6a1511
@little__prince <13510222833@139.com>
2023-10-25 19:09:25 +0800

    finally solve bean problem

--- Assignment5/ProductManagement/src/main/java/whu/edu/ProductManagement/CodeGenerator.java
+++ Assignment5/ProductManagement/src/main/java/whu/edu/ProductManagement/CodeGenerator.java
@@ -1,4 +1,5 @@
-package codegen;
+package whu.edu.ProductManagement;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
@@ -6,7 +7,7 @@ import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 
-public class CodeGen {
+public class CodeGenerator {
 
     public static void main(String[] args) {
         //创建代码生成器
@@ -43,27 +44,28 @@ public class CodeGen {
     private static PackageConfig packageConfig() {
         PackageConfig packageInfo = new PackageConfig();
         //设置生成的包名，与代码所在位置不冲突， 二者叠加组成完整路径
-        packageInfo.setParent("whu.edu.Product.Management");
+        packageInfo.setParent("whu.edu.ProductManagement");
         //设置实体类包名
-        packageInfo.setEntity("Models");
+        packageInfo.setEntity("domain");
         //设置数据层包名
-        packageInfo.setMapper("User");
+        packageInfo.setMapper("dao");
         return packageInfo;
     }
 
     private static GlobalConfig globalConfig() {
         GlobalConfig globalConfig = new GlobalConfig();
         //设置代码生成位置
+        String s = System.getProperty("user.dir");
         globalConfig.setOutputDir(System.getProperty("user.dir")
-                +"/Product-Management/src/main/java");
+                + "/ProductManagement/src/main/java");
         //设置生成完毕后是否打开生成代码所在的目录
         globalConfig.setOpen(false);
         //设置作者
-        globalConfig.setAuthor("little_prince");
+        globalConfig.setAuthor("little prince");
         //设置是否覆盖原始生成的文件
         globalConfig.setFileOverride(true);
         //设置数据层接口名，%s为占位符，指代模块名称
-        globalConfig.setMapperName("I%s");
+        globalConfig.setMapperName("%sDao");
         //设置Id生成策略
         globalConfig.setIdType(IdType.AUTO);
         return globalConfig;
